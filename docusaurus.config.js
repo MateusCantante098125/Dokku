9ec27d9396b45bf9a10f0c88f76e49ef8e4e0dca
Mateus Cantante <2004mateus@gmail.com>
2025-05-16 10:20:26 +0100

    Correção do url do site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/dokku/',
+  baseUrl: '/Dokku/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'MateusCantante098125', // Usually your GitHub org/user name.
-  projectName: 'dokku', // Usually your repo name.
+  projectName: 'Dokku', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
